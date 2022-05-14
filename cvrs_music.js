e05bfc7db4a766919cd0e09117260b6576bd8e40
HACKER3000 <hacker3000@posteo.org>
2022-05-15 00:12:07 +0200

    basic stuff works

---
+++ cvrs_music.js
@@ -0,0 +1,217 @@
+/* album gallery */
+var mp_gallery = document.getElementById('mp_gallery');
+crazy_albums.forEach((album, album_idx) => {
+	let album_div = document.createElement('div');
+	album_div.className = 'crazy_album';
+	album_div.style.backgroundImage = 'url(' + album.album_cover + ')';
+	
+	let album_info_div = document.createElement('summary');
+	album_info_div.className = 'crazy_album_info';
+	album_info_div.innerHtml = `<p>🐱 <a href="${album.album_url}">${album.album_artist} - ${album.album_name}</a></p>`;
+	
+	
+	let tracklist_div = document.createElement('ul');
+	tracklist_div.className = 'crazy_tracklist';
+	album.album_tracks.forEach((track, track_idx) => {
+		let track_div = document.createElement('li');
+		let track_btn = document.createElement('button');
+		
+		track_btn.addEventListener('click', e => {select_track(album_idx,track_idx);});
+		track_btn.innerText = (track.track_artist || album.album_artist) + ' - ' + track.track_name;
+		track_div.appendChild(track_btn);
+		
+		tracklist_div.appendChild(track_div);
+	});
+	album_div.appendChild(tracklist_div);
+	mp_gallery.appendChild(album_div);
+});
+
+var mp_audio = document.getElementById('mp_audio');
+
+/* playlist logic */
+var music_credits = document.getElementById('music_credits');
+var mp_audio_src = document.getElementById('mp_audio_src');
+
+var mp_use_hd_audio = false;
+var mp_track_index = 0;
+var mü_album_index = 0;
+
+function select_track(album,track) {
+	mp_album_index = album;
+	mp_track_index = track;
+	
+	play_track_index();
+}
+
+function change_track(d) {
+	if (d) {
+		mp_track_index++;
+		if (mp_track_index >= crazy_albums[mp_album_index].length) mp_track_index = 0;
+	}
+	else {
+		mp_track_index--;
+		if (mp_track_index < 0) mp_track_index = crazy_albums[mp_album_index].length-1;
+	}
+	
+	play_track_index();
+}
+
+function play_track_index() {	
+	music_credits.innerText = (crazy_albums[mp_album_index]['album_tracks'][mp_track_index]['track_artist'] || crazy_albums[mp_album_index]['album_artist']) + ' - ' + crazy_albums[mp_album_index]['album_tracks'][mp_track_index]['track_name'];
+	music_credits.href = crazy_albums[mp_album_index]['album_tracks'][mp_track_index]['track_url'] || crazy_albums[mp_album_index]['album_url'];
+	
+	let audio_url = crazy_albums[mp_album_index]['album_tracks'][mp_track_index]['m4a_url'];
+	
+	if (mp_use_hd_audio && typeof crazy_albums[mp_album_index]['album_tracks'][mp_track_index]['flac_url'] == 'string') {
+		audio_url = crazy_albums[mp_album_index]['album_tracks'][mp_track_index]['flac_url'];
+	}
+	
+	mp_audio.src=audio_url;
+	mp_audio.load();
+	mp_audio.play();
+}
+
+
+mp_audio.onended = function(){change_track(true);};
+
+
+/* visualizer */
+var mp_header = document.getElementById('mp_header');
+var mp_audio_vis = document.getElementById('mp_audio_vis');
+var mp_audio_vis_2 = document.getElementById('mp_audio_vis_2');
+
+mp_audio_vis.width = mp_header.clientWidth;
+mp_audio_vis.height = mp_header.clientHeight;
+
+var mp_vis_ctx = mp_audio_vis.getContext("2d");
+mp_vis_ctx.strokeStyle = 'whitesmoke';
+
+const audioContext = new AudioContext();
+const audiosource_bgm = audioContext.createMediaElementSource(mp_audio);
+var analyser_bgm = audioContext.createAnalyser();
+analyser_bgm.fftSize = Math.pow(2, Math.floor(Math.log(mp_audio_vis_2.clientWidth)/Math.log(2) + 1)) * 2; //round up to the nearest power of 2
+var mp_buf = new Uint8Array(analyser_bgm.frequencyBinCount);
+var mp_freq = new Uint8Array(analyser_bgm.frequencyBinCount);
+audiosource_bgm.connect(analyser_bgm);
+analyser_bgm.connect(audioContext.destination);
+
+mp_audio_vis_2.width =mp_audio_vis_2.clientWidth;
+mp_audio_vis_2.height = 128;
+var mp_vis_ctx_2 = mp_audio_vis_2.getContext("2d");
+mp_vis_ctx_2.strokeStyle = 'whitesmoke';
+
+
+var mp_vis_fps = 0;
+var mp_vis_frames = 0;
+var mp_vis_cycle = 0;
+var mp_vis_div = 0;
+function vis_audio() {
+	//fps limit
+	mp_vis_cycle++; //adding before testing is mitigated by the <=
+	if(mp_vis_cycle <= mp_vis_div) {
+		/* divide framerate to save CPU. most LCDs dont display full framerate well anyways */
+		requestAnimationFrame(vis_audio);
+		return;
+	}
+	
+	mp_vis_cycle = 0;
+	
+	mp_vis_ctx.clearRect(0,0,mp_audio_vis.width,mp_audio_vis.height);
+	mp_vis_ctx.beginPath();
+	//mp_vis_ctx.strokeStyle = 'whitesmoke';
+	mp_vis_ctx.lineWidth = 2;
+	
+	
+	analyser_bgm.getByteTimeDomainData(mp_buf);
+	for (let x = 0; x < mp_audio_vis.width; x++) {
+		let y = mp_buf[x] * (mp_audio_vis.height / 255);
+		
+		if (x == 1) {
+				mp_vis_ctx.moveTo(x,y);
+		}
+		else {
+				mp_vis_ctx.lineTo(x,y);
+		}
+	}
+	
+	mp_vis_ctx.stroke();
+	
+	
+	mp_vis_ctx_2.clearRect(0,0,mp_audio_vis.width,mp_audio_vis.height);
+	mp_vis_ctx_2.beginPath();
+	//mp_vis_ctx_2.strokeStyle = 'whitesmoke';
+	mp_vis_ctx_2.lineWidth = 2;
+	
+	analyser_bgm.getByteFrequencyData(mp_freq);
+	for (let x = 0; x < mp_audio_vis_2.width; x++) {
+		let y = (mp_buf[x] / 2);
+		
+		if (x == 1) {
+				mp_vis_ctx.moveTo(x,y);
+		}
+		else {
+				mp_vis_ctx.lineTo(x,y);
+		}
+	}
+	
+	mp_vis_ctx_2.stroke();
+	
+	
+	mp_vis_frames++;
+	requestAnimationFrame(vis_audio);
+}
+vis_audio();
+
+mp_audio.onwaiting = mp_audio.onstalled = function () {mp_vis_ctx.strokeStyle = '#333'; mp_vis_ctx_2.strokeStyle = '#333';}
+mp_audio.onplaying = function () {mp_vis_ctx.strokeStyle = 'whitesmoke'; mp_vis_ctx_2.strokeStyle = 'whitesmoke';}
+mp_audio.onerror = function () {mp_vis_ctx.strokeStyle = 'red';mp_vis_ctx_2.strokeStyle = 'red';}
+
+//measure fps
+var mp_performance = document.getElementById('mp_fps');
+setInterval(function(){
+	mp_vis_fps = mp_vis_frames;
+	mp_vis_frames = 0;
+	mp_fps.innerText = mp_vis_fps + "FPS";
+}, 1000);
+
+
+/* controls */
+var mp_performance = document.getElementById('mp_performance');
+var mp_next = document.getElementById('mp_next');
+var mp_prev = document.getElementById('mp_prev');
+var mp_hd = document.getElementById('mp_hd');
+
+function mp_cycle_fps() {
+	mp_vis_div += 1;
+	if (mp_vis_div >= 4) mp_vis_div=0;
+	mp_performance.innerText = (mp_vis_div!=0) ? "1/" + (mp_vis_div+1) : "max";
+}
+
+mp_performance.onclick = mp_cycle_fps;
+mp_performance.innerText = "max";
+mp_next.onclick = function(){change_track(true)};
+mp_prev.onclick = function(){change_track(false)};
+mp_hd.onclick = function(){
+	if(mp_track_index<0) {
+		alert("No HD version available for eastereggs. I dont want to implement that.");
+		return;
+	}
+	
+	mp_use_hd_audio = !mp_use_hd_audio;
+	mp_hd.innerText = mp_use_hd_audio ? 'HD' : 'SD';
+	
+	let mp_pos_on_sw = mp_audio.currentTime;
+	if (typeof crazy_albums[mp_album_index]['album_tracks'][mp_track_index]['hd_url'] == 'string') {
+		if (mp_use_hd_audio) {
+				mp_audio_src.src=mp_audio.src=crazy_albums[mp_album_index]['album_tracks'][mp_track_index]['hd_url'];
+		}
+		else {
+				mp_audio_src.src=mp_audio.src=crazy_albums[mp_album_index]['album_tracks'][mp_track_index]['audio_url'];
+		}
+		
+		mp_audio.load();
+		mp_audio.play().then(p => {mp_audio.currentTime = mp_pos_on_sw;});
+	}
+	
+}
+mp_audio.onloadeddata = function() {mp_hd.style.borderColor = (typeof crazy_albums[mp_album_index]['album_tracks'][mp_track_index]['hd_url'] == 'string' && mp_use_hd_audio) ? '#0FF' : '#FFF';};
---
+++ musics_list.js
@@ -0,0 +1,121 @@
+//doing this as js instead of plain json makes debugging locally easier
+
+var crazy_albums = [
+	{
+		'album_artist': 'cvrsed',
+		'album_name': ' yokubari​-​cats!',
+		'album_url': 'https://cvrsed.bandcamp.com/album/yokubari-cats',
+		'album_cover': 'https://f4.bcbits.com/img/a0879793575_16.jpg',
+		'album_tracks': [
+			{
+				'track_name': 'nevafeltthisway',
+				'track_m4a': '',
+				'track_flac': ''
+			},
+			{
+				'track_name': 'thxuevryday',
+				'track_m4a': '',
+				'track_flac': ''
+			},
+			{
+				'track_name': 'luv2shy',
+				'track_m4a': '',
+				'track_flac': ''
+			}
+		]
+	},
+	{
+		'album_artist': 'cvrsed',
+		'album_name': 'possibilities limitless',
+		'album_url': 'https://cvrsed.bandcamp.com/album/possibilities-limitless',
+		'album_cover': 'https://f4.bcbits.com/img/a1830718248_16.jpg',
+		'album_tracks': [
+			{
+				'track_name': '3LGY3M',
+				'track_m4a': '',
+				'track_flac': ''
+			},
+			{
+				'track_name': 'P1NK',
+				'track_m4a': '',
+				'track_flac': ''
+			},
+			{
+				'track_name': 'W1LD',
+				'track_artist': 'lovulovuelovue + cvrsed',
+				'track_m4a': '',
+				'track_flac': ''
+			},
+			{
+				'track_name': '1ST_TRY',
+				'track_m4a': '',
+				'track_flac': ''
+			},
+			{
+				'track_name': 'CRYSIS',
+				'track_m4a': '',
+				'track_flac': ''
+			},
+			{
+				'track_name': '1NF1N1TY',
+				'track_m4a': '',
+				'track_flac': ''
+			},
+			{
+				'track_name': 'XOXOXO',
+				'track_artist': 'cvrsed + alongthewalls',
+				'track_m4a': '',
+				'track_flac': ''
+			},
+			{
+				'track_name': '',
+				'track_url': 'https://cvrsed.bandcamp.com/track/deepweb',
+				'track_m4a': '',
+				'track_flac': ''
+			}
+		]
+	},
+	{
+		'album_artist': 'cvrsed',
+		'album_name': 'oxymoronic.txtfiles',
+		'album_cover': 'https://f4.bcbits.com/img/a3560193762_10.jpg',
+		'album_url': 'https://cvrsed.bandcamp.com/album/oxymoronic-txtfiles',
+		'album_tracks': [
+			{
+				'track_name': '',
+				'track_m4a': '',
+				'track_flac': ''
+			},
+			{
+				'track_name': '',
+				'track_m4a': '',
+				'track_flac': ''
+			},
+			{
+				'track_name': '',
+				'track_m4a': '',
+				'track_flac': ''
+			},
+			{
+				'track_name': '',
+				'track_m4a': '',
+				'track_flac': ''
+			},
+			{
+				'track_name': '',
+				'track_m4a': '',
+				'track_flac': ''
+			},
+			{
+				'track_name': '',
+				'track_m4a': '',
+				'track_flac': ''
+			},
+			{
+				'track_name': '',
+				'track_m4a': '',
+				'track_flac': ''
+			}
+		]
+	}
+]
